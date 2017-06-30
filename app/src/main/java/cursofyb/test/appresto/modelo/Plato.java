d7b7752e5eb2f95b59080e1de859284eed188275
martdominguez <mdomingu@gmail.com>
2017-06-29 23:52:32 -0300

    version 1

---
+++ app/src/main/java/cursofyb/test/appresto/modelo/Plato.java
@@ -0,0 +1,14 @@
+package cursofyb.test.appresto.modelo;
+
+/**
+ * Created by martdominguez on 29/06/2017.
+ */
+
+public class Plato {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private TipoPlato tipo;
+    private Double precio;
+
+}
---
+++ app/src/main/java/cursofyb/test/appresto/modelo/TipoPlato.java
@@ -0,0 +1,10 @@
+package cursofyb.test.appresto.modelo;
+
+/**
+ * Created by martdominguez on 29/06/2017.
+ */
+
+public class TipoPlato {
+    private Integer id;
+    private String nombre;
+}
